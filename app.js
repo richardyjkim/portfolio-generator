da654f2bf9b9d7f6821ba6f842f062e85f6a0bb8
Richard <richardyjkim@gmail.com>
2021-03-22 15:57:03 -0700

    modified

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = (profileDataArr) => {
-  for (let i=0; i <profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('====================');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
- printProfileData(profileDataArgs);
- 
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
@@ -36,5 +20,3 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 })
-
->>>>>>> feature/generate-webpage
